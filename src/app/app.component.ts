12734ecca6aa82955ae3c31e79b24edfb5004702
Vlad Dunaev <71637695+neverdreamy@users.noreply.github.com>
2020-11-17 15:14:17 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,8 @@ export interface Post {
 export class AppComponent {
 
   posts: Post[] = [
-    {title: 'title1', text: 'text1', id: 1, length: 6},
-    {title: 'title2', text: 'text2', id: 2, length: 6}
+    {title: 'title1', text: 'text1', id: 1, length: 5},
+    {title: 'title2', text: 'text2', id: 2, length: 5}
   ];
 
   updatePosts(myPost: Post) {
