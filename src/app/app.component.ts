aef1b73ee50fcd5a16d6487c83b394657f806906
neverdreamy <neverdreamy@gmail.com>
2020-11-29 23:09:56 +0300

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import {Component} from '@angular/core';
+import {Observable} from 'rxjs';
+
+export interface Post {
+  title: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,5 +13,51 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
-  isVisible = true;
+  num = 2.51234;
+  str = 'hello world';
+  date: Date = new Date();
+  float = 0.42;
+
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 3,
+        f: 4
+      }
+    }
+  };
+
+  search = '';
+  searchField = 'title';
+
+  promise: Promise<string> = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('Promise resolved')
+    }, 6000);
+  });
+
+  myDate: Observable<Date> = new Observable( obs => {
+    setInterval(() => {
+      obs.next(new Date());
+    }, 1000);
+  });
+
+  posts: Post[] = [
+    {title: 'Game', text: 'Game of the year'},
+    {title: 'Film', text: 'Film of the year'},
+    {title: 'Web', text: 'HTML + CSS + JS'}
+  ];
+
+  addPost() {
+    this.posts.unshift({
+      title: 'Post title',
+      text: 'Post text'
+    });
+  }
+
+  removePost() {
+    this.posts.shift();
+  }
 }
