0100cbb5af6f449ee7948b0a5c7a1a6bcf9587cc
neverdreamy <neverdreamy@gmail.com>
2020-12-27 22:03:51 +0300

    forms and validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { CounterComponent } from './counter/counter.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    CommonModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
